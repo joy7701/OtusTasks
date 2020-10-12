db879fafcc4fe85a7567b0566fc815197ec93497
joy7701 <roflan1996@gmail.com>
2020-10-12 19:12:55 +0300

    Reworked ipv4 filter

--- Otus02/src/ip_filter.cpp
+++ Otus02/src/ip_filter.cpp
@@ -2,37 +2,55 @@
 #include <string>
 #include <sstream>
 #include <cstddef> // std::size_t
+#include <cstdint> // providing fixed width integer types
 #include <vector>
 #include <algorithm>
 #include <functional>
 
 class ipv4 {
 public:
-	ipv4() : valid{false} {} // default constructor
-	ipv4(const std::string ip_address) : valid{false}
+	std::uint32_t address = 0; // contains ipv4 address
+    explicit ipv4(const std::string ip_address)
 	{
-		split_string(ip_address);
+		make_ip(ip_address);
 	}
 	bool check_valid() { return valid; }
-	std::vector<int> data;
 
+	friend    std::ostream& operator<<(std::ostream& os, const ipv4& ip)
+    	{
+        	// shift left bits and left only first significant byte
+        	std::uint32_t part1 = (ip.address >> 24) & 0xFF;
+        	std::uint32_t part2 = (ip.address >> 16) & 0xFF;
+        	std::uint32_t part3 = (ip.address >> 8) & 0xFF;
+        	std::uint32_t part4 = ip.address & 0xFF;
+        	os << part1 << '.' << part2 << '.' << part3 << '.' << part4;
+		return os;
+	}
+	friend bool operator<(const ipv4& a, const ipv4& b)
+	{
+		return	a.address < b.address;
+	}
+    friend bool operator>(const ipv4& a, const ipv4& b)
+    {
+        return !(a < b);
+    }
 private:
-	bool valid; // valid ip adress: consists of 4 parts
+	bool valid = false; // valid ip adress: consists of 4 parts
 
-	void split_string(std::string source)
+	void make_ip(std::string source)
 	{
 		std::istringstream ss(source);
 		std::string digit;
-		int counter = 0;
+		std::uint32_t counter = 0;
+		address = 0;
 		while (std::getline(ss, digit, '.'))
 		{
-			int value;
+			std::uint32_t current_part;
 			std::stringstream css(digit);
-
-			if((css >> value) && (value >= 0 && value <= 255)) // if operation not fails and [0;255]
+			if((css >> current_part) && (current_part >= 0 && current_part <= 255)) // if operation not fails and [0;255]
 			{
-				counter++;
-				data.push_back(value); // load current part of ipv4
+				address = (address << 8) | current_part;
+				++counter;
 			}
 		}
 		if (counter == 4)
@@ -40,74 +58,16 @@ private:
 	}
 };
 
-std::ostream& operator<<(std::ostream& os, const ipv4& ip)
-{
-	os << ip.data[0] << '.' << ip.data[1] << '.' << ip.data[2] << '.' << ip.data[3];
-	return os;
-}
-
-auto ipv4_less_lambda = [](ipv4 a, ipv4 b) 
-{
-	if (a.data[0] < b.data[0]) // 1st part "<"
-		return true;
-	else
-	{
-		if (a.data[0] > b.data[0])// ">"
-			return false;
-		else // 2nd part; if current are equal
-		{
-			if (a.data[1] < b.data[1]) // 2nd part "<"
-				return true;
-			else
-			{
-				if (a.data[1] > b.data[1]) // ">"
-					return false;
-				else // 3nd part; if current are equal
-				{
-					if (a.data[2] < b.data[2]) // "<"
-						return true;
-					else
-					{
-						if (a.data[2] > b.data[2]) // ">"
-							return false;
-						else
-						{
-							if (a.data[3] < b.data[3]) // "<"
-								return true;
-							else
-							{
-								if (a.data[3] > b.data[3])
-									return false;
-								else
-								{
-									return true;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	if(a.data[2] < b.data[2]) // 3rd pard
-		return true;
-	else if (a.data[3] < b.data[3]) // 4th part
-		return true;
-	else
-		return false;
-};
-
-int main() 
+int main()
 {
 	std::vector<ipv4> ipv4_vector; // a vector contains our ipv4 addresses
 
-	// preprocessing input 
+	// preprocessing input
 	for (std::string line; std::getline(std::cin, line);)
 	{
 		//remove useless information
 		std::size_t found = line.find_first_of("\t");
-		
+
 		if (found == std::string::npos) // if \t not found terminate program
 			return 0;
 
@@ -115,14 +75,15 @@ int main()
 
 		// construct ipv4 from this string
 		ipv4 curr_ipv4{ line };
-		
+
 		// pushing ipv4 to the vector
 		if (curr_ipv4.check_valid())
 		{
 			ipv4_vector.push_back(curr_ipv4);
 		}
 	}
-	std::sort(ipv4_vector.begin(), ipv4_vector.end(), std::not_fn(ipv4_less_lambda)); // sorting in decreasing order
+
+	std::sort(ipv4_vector.begin(), ipv4_vector.end(), [](ipv4 a, ipv4 b){ return a > b; }); // sorting in decreasing order
 
 	// print all sorted elements
 	for (auto cur_ip : ipv4_vector)
@@ -131,24 +92,28 @@ int main()
 	}
 
 	// elements are already sorted so just check other conditions
-	
+
 	// print elements where first byte is equal 1
 	for (auto cur_ip : ipv4_vector)
 	{
-		if (cur_ip.data[0] == 1)
+		if ((cur_ip.address >> 24) == 1)
 			std::cout << cur_ip << std::endl;
 	}
 
 	// print elements where first byte is equal 46 & second byte is equal 70
 	for (auto cur_ip : ipv4_vector)
 	{
-		if (cur_ip.data[0] == 46 && cur_ip.data[1] == 70)
+		if ((cur_ip.address >> 24) == 46 && ((cur_ip.address >> 16) & 0xFF) == 70)
 			std::cout << cur_ip << std::endl;
 	}
 	// print elements where any of bytes are equal 46
 	for (auto cur_ip : ipv4_vector)
 	{
-		if (cur_ip.data[0] == 46 || cur_ip.data[1] == 46 || cur_ip.data[2] == 46 || cur_ip.data[3] == 46)
-			std::cout << cur_ip << std::endl;
+		if ((cur_ip.address & 0xFF) == 46 ||
+            ((cur_ip.address >> 8) & 0xFF) == 46 ||
+            ((cur_ip.address >> 16) & 0xFF) == 46 ||
+            ((cur_ip.address >> 24) & 0xFF) == 46
+           )
+		    std::cout << cur_ip << std::endl;
 	}
 }
